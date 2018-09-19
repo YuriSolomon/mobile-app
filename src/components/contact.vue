c6230c168dca840cebe8b9948b3ac87e31de2f9c
YuriSolomon <juristein21@gmail.com>
2018-09-19 17:12:10 +0200

    build locations and contact pages

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="contact">
     <h4>Contact</h4>
-    
+    <p>Please email us at <a href="mailto:nysl@chisoccer.org">nysl@chisoccer.org</a> </p>
+    <p>We will reply to your email as soon as we can.</p>
   </div>
 </template>
 
--- src/components/locations.vue
+++ src/components/locations.vue
@@ -2,7 +2,18 @@
   <div class="locations">
     <h4>Locations</h4>
     <div class="container">
-
+        <h5>AJ Katzenmaier Elementary</h5>
+        <p>24 W. Walton St., Chicago, IL 60610</p>
+        <h5>Greenbay Elementary</h5>
+        <p>1734 N. Orleans St., Chicago, IL 60614</p>
+        <h5>Howard A Yeager Elementary</h5>
+        <p>2245 N. Southport Ave., Chicago, IL 60614</p>
+        <h5>Marjorie P Hart Elementary</h5>
+        <p>2625 N. Orchard St., Chicago, IL 60614</p>
+        <h5>North Elementary</h5>
+        <p>1409 N. Ogden Ave., Chicago, IL 60610</p>
+        <h5>South Elementary</h5>
+        <p>2101 N. Fremont St., Chicago, IL 60614</p>
     </div>
   </div>
 </template>
@@ -15,9 +26,19 @@ export default {
 
 <style scoped>
 .locations {
-  color: white;
+    color: white;
 }
 h4 {
-  margin-top: 70px;
+    margin-top: 70px;
+}
+h5 {
+    text-align: left;
+    text-decoration: underline;
+    margin-top: 30px;
+    background: rgb(33, 37, 41, 0.7)!important;
+}
+P {
+    text-align: left;
+    background: rgb(33, 37, 41, 0.7)!important;
 }
 </style>
