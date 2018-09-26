b260e240231787177357ee98d94357358eaff33c
YuriSolomon <juristein21@gmail.com>
2018-09-26 18:10:39 +0200

    get teams online

--- src/components/teams.vue
+++ src/components/teams.vue
@@ -3,24 +3,48 @@
     <h4>Teams</h4>
     <div class="container">
         <div class="row d-flex justify-content-around">
-            <div class="square m-4 d-flex align-items-center justify-content-center"><h1>U1</h1></div>
-            <div class="square m-4 d-flex align-items-center justify-content-center"><h1>U2</h1></div>
-        </div>
-        <div class="row d-flex justify-content-around">
-            <div class="square m-4 d-flex align-items-center justify-content-center"><h1>U3</h1></div>
-            <div class="square m-4 d-flex align-items-center justify-content-center"><h1>U4</h1></div>
-        </div>
-        <div class="row d-flex justify-content-around">
-            <div class="square m-4 d-flex align-items-center justify-content-center"><h1>U5</h1></div>
-            <div class="square m-4 d-flex align-items-center justify-content-center"><h1>U6</h1></div>
+            <div v-for="(team,i) in teams" :key="i" class="square m-4 d-flex align-items-center justify-content-center">
+              <h1>{{team}}</h1>
+            </div>
         </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
-  name: "Teams"
+  name: "Teams",
+  data() {
+    return {
+      teams1: [],
+      teams2: [],
+      teams1and2: [],
+      sortedTeams: [],
+      teams: []
+    };
+  },
+  created() {
+    this.teams1 = this.games.map(team => team.team1);
+    this.teams2 = this.games.map(team => team.team2);
+    this.teams1and2 = this.teams1.concat(this.teams2);
+    this.sortedTeams = this.teams1and2.sort();
+    this.sortedTeams.forEach((team) => {
+      if (!(team == (team+1))) {
+        if (!this.teams.includes(team)) {
+          this.teams.push(team)
+        }
+      }
+    });
+    // console.log(this.sortedTeams);
+    // console.log(this.teams);
+  },
+  computed: {
+    ...mapState({
+      games: "games"
+    })
+  }
 };
 </script>
 
