11922f7f4b5efe1f0ccdc3e0d3cbb8421f116aea
YuriSolomon <juristein21@gmail.com>
2018-09-27 11:45:50 +0200

    get loop through teams to get teams info

--- src/components/teams.vue
+++ src/components/teams.vue
@@ -3,7 +3,7 @@
     <h4>Teams</h4>
     <div class="container">
         <div class="row d-flex justify-content-around">
-            <div v-for="(team,i) in teams" :key="i" class="square m-4 d-flex align-items-center justify-content-center">
+            <div v-for="(team,i) in teams" v-on:click="getTeamInfo" :key="i" class="square m-4 d-flex align-items-center justify-content-center">
               <h1>{{team}}</h1>
             </div>
         </div>
@@ -18,32 +18,45 @@ export default {
   name: "Teams",
   data() {
     return {
-      teams1: [],
-      teams2: [],
-      teams1and2: [],
-      sortedTeams: [],
-      teams: []
+      teams: [],
+      teamInfo: []
     };
   },
   created() {
-    this.teams1 = this.games.map(team => team.team1);
-    this.teams2 = this.games.map(team => team.team2);
-    this.teams1and2 = this.teams1.concat(this.teams2);
-    this.sortedTeams = this.teams1and2.sort();
-    this.sortedTeams.forEach((team) => {
-      if (!(team == (team+1))) {
-        if (!this.teams.includes(team)) {
-          this.teams.push(team)
-        }
-      }
-    });
-    // console.log(this.sortedTeams);
-    // console.log(this.teams);
+    this.getTeams()
   },
   computed: {
     ...mapState({
       games: "games"
     })
+  },
+  methods:{
+    getTeams() {
+      let teams1 = this.games.map(team => team.team1);
+      let teams2 = this.games.map(team => team.team2);
+      let teams1and2 = teams1.concat(teams2);
+      let sortedTeams = teams1and2.sort();
+      sortedTeams.forEach((team) => {
+        if (!(team == (team+1))) {
+          if (!this.teams.includes(team)) {
+            this.teams.push(team)
+          }
+        }
+      });
+      // console.log(this.sortedTeams);
+      // console.log(this.teams);
+    },
+    getTeamInfo() {
+      this.teamInfo = [];
+      this.teams.forEach(team =>{
+        this.games.forEach(game => {
+          if ((game.team1 == team) || (game.team2 == team)) {
+            this.teamInfo.push(game);
+          }
+        })
+      })
+      console.log(this.teamInfo);
+    }
   }
 };
 </script>
